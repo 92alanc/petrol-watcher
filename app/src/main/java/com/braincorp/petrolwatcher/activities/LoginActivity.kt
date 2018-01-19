646517dd14f65fcbd59a53a62fc7025322f09a62
Alan Camargo <alan.uk.br@gmail.com>
2018-01-19 16:48:23 -0200

    Added checkAuthentication to LoginActivity

--- app/src/main/java/com/braincorp/petrolwatcher/activities/LoginActivity.kt
+++ app/src/main/java/com/braincorp/petrolwatcher/activities/LoginActivity.kt
@@ -3,12 +3,20 @@ package com.braincorp.petrolwatcher.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.braincorp.petrolwatcher.R
+import com.braincorp.petrolwatcher.authentication.AuthenticationManager
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        checkAuthentication()
+    }
+
+    private fun checkAuthentication() {
+        if (AuthenticationManager.USER != null) {
+            // TODO: open home activity
+        }
     }
 
 }
