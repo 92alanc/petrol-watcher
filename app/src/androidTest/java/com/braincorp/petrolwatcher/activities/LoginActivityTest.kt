3f8055e403844622767d87789a888ba7f1252971
Alan Camargo <alan.uk.br@gmail.com>
2018-01-21 21:29:28 -0200

    Finished LoginActivityTest

--- app/src/androidTest/java/com/braincorp/petrolwatcher/activities/LoginActivityTest.kt
+++ app/src/androidTest/java/com/braincorp/petrolwatcher/activities/LoginActivityTest.kt
@@ -1,6 +1,7 @@
 package com.braincorp.petrolwatcher.activities
 
 import android.support.test.espresso.intent.Intents
+import android.support.test.filters.FlakyTest
 import android.support.test.runner.AndroidJUnit4
 import com.braincorp.petrolwatcher.authentication.AuthenticationManager
 import com.braincorp.petrolwatcher.robots.action.LoginActivityActionRobot
@@ -13,6 +14,10 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LoginActivityTest {
+
+    private companion object {
+        const val DEFAULT_TIMEOUT = 5000L
+    }
 
     private val robot = LoginActivityActionRobot()
 
@@ -27,22 +32,25 @@ class LoginActivityTest {
 
     @After
     fun after() {
+        AuthenticationManager.signOut()
+        robot.wait(2000)
         Intents.release()
     }
 
-    @Test
+    @FlakyTest(detail = "Sometimes there's not enough time to receive the authentication server response")
+    @Test(timeout = DEFAULT_TIMEOUT)
     fun shouldLaunchHomeActivityWithCorrectEmailAndPassword() {
         robot.typeEmail(correct = true)
                 .hideKeyboard()
         robot.typePassword(correct = true)
                 .hideKeyboard()
         robot.clickOnSignIn()
-                .wait(1000)
+                .wait(2000)
         checkIfLaunchesHomeActivity()
-        AuthenticationManager.signOut()
     }
 
-    @Test
+    @FlakyTest(detail = "Sometimes there's not enough time to receive the authentication server response")
+    @Test(timeout = DEFAULT_TIMEOUT)
     fun shouldShowErrorDialogueWithIncorrectEmail() {
         robot.typeEmail(correct = false)
                 .hideKeyboard()
@@ -53,12 +61,52 @@ class LoginActivityTest {
         checkIfShowsErrorDialogue()
     }
 
-    @Test
+    @FlakyTest(detail = "Sometimes there's not enough time to receive the authentication server response")
+    @Test(timeout = DEFAULT_TIMEOUT)
     fun shouldShowErrorDialogueWithIncorrectPassword() {
         robot.typeEmail(correct = true)
                 .hideKeyboard()
         robot.typePassword(correct = false)
                 .hideKeyboard()
+        robot.clickOnSignIn()
+                .wait()
+        checkIfShowsErrorDialogue()
+    }
+
+    @FlakyTest(detail = "Sometimes there's not enough time to receive the authentication server response")
+    @Test(timeout = DEFAULT_TIMEOUT)
+    fun shouldShowErrorDialogueWithIncorrectEmailAndPassword() {
+        robot.typeEmail(correct = false)
+                .hideKeyboard()
+        robot.typePassword(correct = false)
+                .hideKeyboard()
+        robot.clickOnSignIn()
+                .wait()
+        checkIfShowsErrorDialogue()
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    fun shouldShowErrorDialogueWithNullEmail() {
+        robot.typePassword(correct = true)
+                .hideKeyboard()
+        robot.clickOnSignIn()
+                .wait()
+        checkIfShowsErrorDialogue()
+    }
+
+    @FlakyTest(detail = "Sometimes there's not enough time to receive the authentication server response")
+    @Test(timeout = DEFAULT_TIMEOUT)
+    fun shouldShowErrorDialogueWithNullPassword() {
+        robot.typeEmail(correct = true)
+                .hideKeyboard()
+        robot.clickOnSignIn()
+                .wait()
+        checkIfShowsErrorDialogue()
+    }
+
+    @FlakyTest(detail = "Sometimes there's not enough time to receive the authentication server response")
+    @Test(timeout = DEFAULT_TIMEOUT)
+    fun shouldShowErrorDialogueWithNullEmailAndPassword() {
         robot.clickOnSignIn()
                 .wait()
         checkIfShowsErrorDialogue()
