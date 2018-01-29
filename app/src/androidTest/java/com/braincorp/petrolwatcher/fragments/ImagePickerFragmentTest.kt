d85af2af1e053201b5e964868df4f46939a2744e
Alan Camargo <alan.uk.br@gmail.com>
2018-01-29 10:50:45 -0200

    Added ImagePickerFragmentTest

---
+++ app/src/androidTest/java/com/braincorp/petrolwatcher/fragments/ImagePickerFragmentTest.kt
@@ -0,0 +1,39 @@
+package com.braincorp.petrolwatcher.fragments
+
+import android.support.test.runner.AndroidJUnit4
+import com.braincorp.petrolwatcher.model.UiMode
+import com.braincorp.petrolwatcher.robots.ImagePickerFragmentRobot
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ImagePickerFragmentTest {
+
+    private val robot = ImagePickerFragmentRobot()
+
+    @Test
+    fun imageViewShouldBeClickableWhenOnCreateMode() {
+        robot.launchFragment(UiMode.CREATE)
+                .checkIfImagePickerIsClickable()
+    }
+
+    @Test
+    fun imageViewShouldBeClickableWhenOnEditMode() {
+        robot.launchFragment(UiMode.EDIT)
+                .checkIfImagePickerIsClickable()
+    }
+
+    @Test
+    fun imageViewShouldNotBeClickableWhenOnViewMode() {
+        robot.launchFragment(UiMode.VIEW)
+                .checkIfImagePickerIsNotClickable()
+    }
+
+    @Test
+    fun shouldShowDialogueWhenClickingOnImageView() {
+        robot.launchFragment(UiMode.CREATE)
+                .clickOnImageView()
+                .checkIfShowsDialogue()
+    }
+
+}
---
+++ app/src/androidTest/java/com/braincorp/petrolwatcher/robots/ImagePickerFragmentRobot.kt
@@ -0,0 +1,50 @@
+package com.braincorp.petrolwatcher.robots
+
+import android.content.Intent
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import com.android21buttons.fragmenttestrule.FragmentTestRule
+import com.braincorp.petrolwatcher.R
+import com.braincorp.petrolwatcher.fragments.ImagePickerFragment
+import com.braincorp.petrolwatcher.model.UiMode
+import org.hamcrest.CoreMatchers.not
+import org.junit.Rule
+
+class ImagePickerFragmentRobot : BaseRobot() {
+
+    @Rule
+    private val rule = FragmentTestRule.create(ImagePickerFragment::class.java,
+            false, false)
+
+    fun launchFragment(uiMode: UiMode): ImagePickerFragmentRobot {
+        rule.launchActivity(Intent())
+        val fragment = ImagePickerFragment.newInstance(uiMode)
+        rule.launchFragment(fragment)
+        return this
+    }
+
+    fun checkIfImagePickerIsClickable(): ImagePickerFragmentRobot {
+        imageView().check(matches(isClickable()))
+        return this
+    }
+
+    fun checkIfImagePickerIsNotClickable(): ImagePickerFragmentRobot {
+        imageView().check(matches(not(isClickable())))
+        return this
+    }
+
+    fun clickOnImageView(): ImagePickerFragmentRobot {
+        click(R.id.imageViewProfile)
+        return this
+    }
+
+    fun checkIfShowsDialogue(): ImagePickerFragmentRobot {
+        onView(withText(R.string.pick_image_from)).check(matches(isDisplayed()))
+        return this
+    }
+
+    private fun imageView(): ViewInteraction = onView(withId(R.id.imageViewProfile))
+
+}
