52ad2302d8af254e3df466dc5315fc79175ce23b
Alan Camargo <alan.uk.br@gmail.com>
2018-01-31 17:20:13 -0200

    Implementing VehiclesActivity

--- app/src/main/java/com/braincorp/petrolwatcher/activities/VehiclesActivity.kt
+++ app/src/main/java/com/braincorp/petrolwatcher/activities/VehiclesActivity.kt
@@ -3,10 +3,14 @@ package com.braincorp.petrolwatcher.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.braincorp.petrolwatcher.R
+import com.braincorp.petrolwatcher.model.UiMode
 import kotlinx.android.synthetic.main.activity_vehicles.*
 
-class VehiclesActivity : BaseActivity() {
+class VehiclesActivity : BaseActivity(), View.OnClickListener {
+
+    // TODO: move the fields from this activity to a fragment
 
     companion object {
         fun getIntent(context: Context): Intent {
@@ -14,11 +18,54 @@ class VehiclesActivity : BaseActivity() {
         }
     }
 
+    private var uiMode = UiMode.VIEW
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vehicles)
+
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        fabVehicles.setOnClickListener(this)
+        prepareUi()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.fabVehicles -> handleFabClick()
+        }
+    }
+
+    private fun handleFabClick() {
+        when (uiMode) {
+            UiMode.VIEW -> prepareCreateMode()
+            else -> save()
+        }
+    }
+
+    private fun prepareUi() {
+        when (uiMode) {
+            UiMode.CREATE -> prepareCreateMode()
+            UiMode.EDIT -> prepareEditMode()
+            UiMode.VIEW -> prepareViewMode()
+        }
+    }
+
+    private fun prepareCreateMode() {
+        // TODO: implement
+    }
+
+    private fun prepareEditMode() {
+        // TODO: implement
+    }
+
+    private fun prepareViewMode() {
+        // TODO: implement
+    }
+
+    private fun save() {
+        // TODO: implement
     }
 
 }
