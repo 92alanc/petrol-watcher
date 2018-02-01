78243adc823687f1aa7cbba2840ef7b61e1411bc
Alan Camargo <alan.uk.br@gmail.com>
2018-02-01 09:16:13 -0200

    Removed metres and yards from DistanceConverter

--- app/src/main/java/com/braincorp/petrolwatcher/utils/DistanceConverter.kt
+++ app/src/main/java/com/braincorp/petrolwatcher/utils/DistanceConverter.kt
@@ -1,25 +1,5 @@
 package com.braincorp.petrolwatcher.utils
 
-fun kilometresToMetres(kilometres: Float) = kilometres * 1000f
-
 fun kilometresToMiles(kilometres: Float) = kilometres * 0.621371f
 
-fun kilometresToYards(kilometres: Float) = kilometres * 1093.61f
-
-fun metresToKilometres(metres: Float) = metres * 0.001f
-
-fun metresToMiles(metres: Float) = metres * 0.000621371f
-
-fun metresToYards(metres: Float) = metres * 1.09361f
-
 fun milesToKilometres(miles: Float) = miles * 1.60934f
-
-fun milesToMetres(miles: Float) = miles * 1609.34f
-
-fun milesToYards(miles: Float) = miles * 1760f
-
-fun yardsToKilometres(yards: Float) = yards * 0.0009144f
-
-fun yardsToMetres(yards: Float) = yards * 0.9144f
-
-fun yardsToMiles(yards: Float) = yards * 0.000568182f
--- app/src/test/java/com/braincorp/petrolwatcher/utils/DistanceConverterTest.kt
+++ app/src/test/java/com/braincorp/petrolwatcher/utils/DistanceConverterTest.kt
@@ -9,13 +9,6 @@ import org.junit.runners.JUnit4
 class DistanceConverterTest {
 
     @Test
-    fun shouldConvertKilometresToMetres() {
-        val expected = 3000f
-        val actual = kilometresToMetres(kilometres = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
     fun shouldConvertKilometresToMiles() {
         val expected = 1.8641129f
         val actual = kilometresToMiles(kilometres = 3f)
@@ -23,72 +16,9 @@ class DistanceConverterTest {
     }
 
     @Test
-    fun shouldConvertKilometresToYards() {
-        val expected = 3280.83f
-        val actual = kilometresToYards(kilometres = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertMetresToMiles() {
-        val expected = 0.0018641129f
-        val actual = metresToMiles(metres = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertMetresToKilometres() {
-        val expected = 0.003f
-        val actual = metresToKilometres(metres = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertMetresToYards() {
-        val expected = 3.2808301f
-        val actual = metresToYards(metres = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
     fun shouldConvertMilesToKilometres() {
         val expected = 4.82802f
         val actual = milesToKilometres(miles = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertMilesToMetres() {
-        val expected = 4828.02f
-        val actual = milesToMetres(miles = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertMilesToYards() {
-        val expected = 5280f
-        val actual = milesToYards(miles = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertYardsToKilometres() {
-        val expected = 0.0027432f
-        val actual = yardsToKilometres(yards = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertYardsToMetres() {
-        val expected = 2.7431998f
-        val actual = yardsToMetres(yards = 3f)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun shouldConvertYardsToMiles() {
-        val expected = 0.0017045459f
-        val actual = yardsToMiles(yards = 3f)
         assertEquals(expected, actual)
     }
 
