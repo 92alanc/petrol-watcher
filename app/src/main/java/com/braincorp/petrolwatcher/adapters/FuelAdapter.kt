b1e055e38962f33a7d1ccae46b864ea34269eaa9
Alan Camargo <alan.uk.br@gmail.com>
2018-02-20 17:59:46 -0300

    Implemented FuelAdapter

---
+++ app/src/main/java/com/braincorp/petrolwatcher/adapters/FuelAdapter.kt
@@ -0,0 +1,55 @@
+package com.braincorp.petrolwatcher.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.braincorp.petrolwatcher.R
+import com.braincorp.petrolwatcher.listeners.OnItemClickListener
+import com.braincorp.petrolwatcher.model.Fuel
+import com.braincorp.petrolwatcher.utils.floatToCurrencyString
+
+class FuelAdapter(private val context: Context,
+                  private val items: MutableSet<Fuel>,
+                  private val onItemClickListener: OnItemClickListener)
+    : RecyclerView.Adapter<FuelAdapter.FuelHolder>() {
+
+    private val list = items.toList()
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): FuelHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.item_fuel, parent, false)
+        return FuelHolder(view, onItemClickListener)
+    }
+
+    override fun onBindViewHolder(holder: FuelHolder?, position: Int) {
+        val fuel = list[position]
+
+        @SuppressLint("SetTextI18n")
+        holder?.textViewFuel?.text = "${fuel.type} (${fuel.quality}:)"
+        holder?.textViewPrice?.text = floatToCurrencyString(fuel.price)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class FuelHolder(itemView: View,
+                     private val onItemClickListener: OnItemClickListener)
+        : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        val textViewFuel = itemView.findViewById<TextView>(R.id.textViewFuel)
+        val textViewPrice = itemView.findViewById<TextView>(R.id.textViewPrice)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            onItemClickListener.onItemClick(layoutPosition)
+        }
+
+    }
+
+}
