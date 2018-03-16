c304a17f94e0f6781a05a3b78336dcf74520dfef
Alan Camargo <alan.uk.br@gmail.com>
2018-03-15 22:44:09 -0300

    Removed unused request code

--- app/src/main/kotlin/com/braincorp/petrolwatcher/utils/RequestCodes.kt
+++ app/src/main/kotlin/com/braincorp/petrolwatcher/utils/RequestCodes.kt
@@ -2,5 +2,4 @@ package com.braincorp.petrolwatcher.utils
 
 const val REQUEST_CODE_LOCATION = 3892
 const val REQUEST_CODE_CAMERA = 3893
-const val REQUEST_CODE_STORAGE = 3894
-const val REQUEST_CODE_GALLERY = 3895
+const val REQUEST_CODE_GALLERY = 3894
