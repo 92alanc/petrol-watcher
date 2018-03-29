2e1cbdf204d369295e47cf0cf03f7e2de60c861f
Alan Camargo <alan.uk.br@gmail.com>
2018-03-29 18:30:57 -0300

    Made vehicle API models parcelable

--- app/src/main/kotlin/com/braincorp/petrolwatcher/feature/vehicles/api/model/Makes.kt
+++ app/src/main/kotlin/com/braincorp/petrolwatcher/feature/vehicles/api/model/Makes.kt
@@ -1,12 +1,42 @@
 package com.braincorp.petrolwatcher.feature.vehicles.api.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Makes(@SerializedName("Makes") val list: List<Make>) {
 
-    data class Make(@SerializedName("make_id") val id: String,
-                    @SerializedName("make_display") val name: String,
-                    @SerializedName("make_is_common") val isCommon: Int,
-                    @SerializedName("make_country") val country: String)
+    data class Make(@SerializedName("make_id") var id: String? = null,
+                    @SerializedName("make_display") var name: String? = null,
+                    @SerializedName("make_is_common") var isCommon: Int = -1,
+                    @SerializedName("make_country") var country: String? = null): Parcelable {
+
+        companion object CREATOR : Parcelable.Creator<Make> {
+            override fun createFromParcel(source: Parcel): Make {
+                return Make(source)
+            }
+
+            override fun newArray(size: Int): Array<Make?> {
+                return arrayOfNulls(size)
+            }
+        }
+
+        constructor(parcel: Parcel): this() {
+            id = parcel.readString()
+            name = parcel.readString()
+            isCommon = parcel.readInt()
+            country = parcel.readString()
+        }
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            dest.writeString(id)
+            dest.writeString(name)
+            dest.writeInt(isCommon)
+            dest.writeString(country)
+        }
+
+        override fun describeContents(): Int = 0
+
+    }
 
 }
--- app/src/main/kotlin/com/braincorp/petrolwatcher/feature/vehicles/api/model/Models.kt
+++ app/src/main/kotlin/com/braincorp/petrolwatcher/feature/vehicles/api/model/Models.kt
@@ -1,10 +1,36 @@
 package com.braincorp.petrolwatcher.feature.vehicles.api.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Models(@SerializedName("Models") val list: List<Model>) {
 
-    data class Model(@SerializedName("model_name") val name: String,
-                     @SerializedName("model_make_id") val makeId: String)
+    data class Model(@SerializedName("model_name") var name: String? = null,
+                     @SerializedName("model_make_id") var makeId: String? = null) : Parcelable {
+
+        companion object CREATOR : Parcelable.Creator<Model> {
+            override fun createFromParcel(source: Parcel): Model {
+                return Model(source)
+            }
+
+            override fun newArray(size: Int): Array<Model?> {
+                return arrayOfNulls(size)
+            }
+        }
+
+        constructor(parcel: Parcel): this() {
+            name = parcel.readString()
+            makeId = parcel.readString()
+        }
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            dest.writeString(name)
+            dest.writeString(makeId)
+        }
+
+        override fun describeContents(): Int = 0
+
+    }
 
 }
