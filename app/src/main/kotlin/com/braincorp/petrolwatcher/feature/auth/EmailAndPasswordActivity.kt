1af38a3411cbbc36f76756cdad43d03a33ade9d5
Alan Camargo <alan.uk.br@gmail.com>
2018-06-25 11:27:29 -0300

    EmailAndPasswordActivity layout ready

--- app/src/main/kotlin/com/braincorp/petrolwatcher/feature/auth/EmailAndPasswordActivity.kt
+++ app/src/main/kotlin/com/braincorp/petrolwatcher/feature/auth/EmailAndPasswordActivity.kt
@@ -3,12 +3,15 @@ package com.braincorp.petrolwatcher.feature.auth
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.braincorp.petrolwatcher.R
+import kotlinx.android.synthetic.main.activity_email_and_password.*
 
 class EmailAndPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email_and_password)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
 }
