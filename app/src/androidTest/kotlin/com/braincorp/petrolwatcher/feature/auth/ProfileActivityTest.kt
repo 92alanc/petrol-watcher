6f4fd1f5115b9902956a7d58e8a3bcdab1a70127
Felipe Luiz <felipe.luiz777@hotmail.com>
2018-07-21 15:53:47 -0300

    Applied changes requested in code review

--- app/src/androidTest/kotlin/com/braincorp/petrolwatcher/feature/auth/ProfileActivityTest.kt
+++ app/src/androidTest/kotlin/com/braincorp/petrolwatcher/feature/auth/ProfileActivityTest.kt
@@ -1,31 +1,14 @@
 package com.braincorp.petrolwatcher.feature.auth
 
-import android.Manifest.permission.CAMERA
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
 import com.braincorp.petrolwatcher.BaseActivityTest
 import com.braincorp.petrolwatcher.feature.auth.imageHandler.MockImageHandler
 import com.braincorp.petrolwatcher.feature.auth.robots.profile
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ProfileActivityTest : BaseActivityTest<ProfileActivity>(ProfileActivity::class.java) {
-
-    @Rule
-    @JvmField
-    val cameraPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(CAMERA)
-
-    @Rule
-    @JvmField
-    val readExternalStoragePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(READ_EXTERNAL_STORAGE)
-
-    @Rule
-    @JvmField
-    val writeExternalStoragePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(WRITE_EXTERNAL_STORAGE)
 
     @Test
     fun whenClickingOnCamera_shouldOpenCamera() {
