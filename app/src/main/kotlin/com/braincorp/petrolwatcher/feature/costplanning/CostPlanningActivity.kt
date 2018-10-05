124922463d29b1d48626b1d38ac5f80236a8daa7
Alan Camargo <alan.uk.br@gmail.com>
2018-10-04 23:05:43 -0300

    Created CostPlanningActivity

---
+++ app/src/main/kotlin/com/braincorp/petrolwatcher/feature/costplanning/CostPlanningActivity.kt
@@ -0,0 +1,17 @@
+package com.braincorp.petrolwatcher.feature.costplanning
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.braincorp.petrolwatcher.R
+
+/**
+ * The activity for cost planning
+ */
+class CostPlanningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cost_planning)
+    }
+
+}
